b6914a52a091e89b40563556a069f7beb6a36530
Alejandro Leal Bermúdez <aleaberm12@gmail.com>
2019-03-30 23:12:18 +0100

    > 1.0.1 / 2019-03-30 - (Fixed) If the status bar is deactivated, the new button won't be displayed.

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # Vivaldi Browser - Mods
 
-![latest-version](https://img.shields.io/badge/Latest%20Version-1.0.0-brightgreen.svg)
+![latest-version](https://img.shields.io/badge/Latest%20Version-1.0.1-brightgreen.svg)
 ![vivaldi-version](https://img.shields.io/badge/Vivaldi%20Version-2.5-brightgreen.svg)
 
 ## List Of Modifications
 
-    - Sync icons in sidebar
-    - Toggle icon for the status bar is added in the sidebar
+    - Sync icons in sidebar.
+    - Toggle icon for the status bar is added in the sidebar.
 
 ## Changelogs
 
+### > 1.0.1 / 2019-03-30
+
+    - (Fixed) If the status bar is deactivated, the new button won't be displayed.
+
 ### > 1.0.0 / 2019-03-30
 
-    - (Added) Sync icons in sidebar
-    - (Added) Toggle icon for the status bar is added in the sidebar
+    - (Added) Sync icons in sidebar.
+    - (Added) Toggle icon for the status bar is added in the sidebar.
--- mods/custom.js
+++ mods/custom.js
@@ -13,8 +13,19 @@
         {
             statusbar.classList.add('disabled-item');
         }
+    }
 
-        console.log('check');
+    function addSeparator()
+    {
+        var sidebar = document.getElementById('switch');
+        var separator = document.createElement('div');
+
+        separator.style.marginTop = 'auto';
+        separator.style.width = '1px';
+
+        sidebar.lastChild.style.marginTop = 0;
+
+        sidebar.insertBefore(separator, sidebar.lastChild);
     }
 
     function moveSyncIcons()
@@ -30,9 +41,10 @@
         var sidebar = document.getElementById('switch');
         var sidebarIcon = document.createElement('button');
 
-        sidebarIcon.style.margin = 'auto 0 -3px 0';
+        sidebarIcon.setAttribute("id", "sidebarIcon");
+        sidebarIcon.style.margin = '0 0 -3px 0';
         sidebarIcon.innerHTML = '<svg style="width: 17px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M32 10l-16-8-16 8 16 8 16-8zM16 4.655l10.689 5.345-10.689 5.345-10.689-5.345 10.689-5.345zM28.795 14.398l3.205 1.602-16 8-16-8 3.205-1.602 12.795 6.398zM28.795 20.398l3.205 1.602-16 8-16-8 3.205-1.602 12.795 6.398z"></path></symbol></svg>';
-        sidebarIcon.addEventListener('click', () => toggleStatusbar());
+        sidebarIcon.addEventListener('click', () => toggleStatusbar(), false);
 
         sidebar.lastChild.style.marginTop = 0;
 
@@ -47,7 +59,13 @@
             return;
         }
 
-        addSidebarIcon();
+        addSeparator();
+
+        if(document.getElementsByClassName('toolbar-statusbar')[0] !== undefined)
+        {
+            addSidebarIcon();
+        }
+
         moveSyncIcons();
     }
 
